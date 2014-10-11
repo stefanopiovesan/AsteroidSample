fef68252191c2ba2f029a2f485a3aeca6c47fc9c
Paolo Scanferla <paolo.scanferla@mondora.com>
2014-10-11 17:25:56 +0200

    Fixed asteroid.

--- app/js/controllers/MyController.js
+++ app/js/controllers/MyController.js
@@ -1,23 +1,9 @@
 'use strict';
 
-eventsApp.controller('MyController',
-    function EventController($scope, myApi) {
-
-        //$scope.history = myApi.getHistory();
-        myApi.on(function (data) {
-            $scope.history.push(data);
-        });
-        $scope.history = [];
-    }
-);
-
 eventsApp.controller('MyAsteroidController',
     function EventController($scope, myAsteroidApi) {
-
-        //$scope.history = myAsteroidApi.getHistory();
-        myAsteroidApi.on(function (data) {
-            $scope.history.push(data);
+        myAsteroidApi.on(function () {
+			$scope.history = myAsteroidApi.getHistory();
         });
-        $scope.history = [];
     }
 );
